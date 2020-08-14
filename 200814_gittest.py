1577f511bf8e38f6d8d8a907f432d3f324b1c0a7
SyogoMochida <syogo1207@gmail.com>
2020-08-14 13:37:39 +0900

    initial commit

---
+++ 200814_gittest.py
@@ -0,0 +1,17 @@
+# git test
+# 2020.08.14 udemy
+
+def my_sum(a,b):
+    
+    return a+b
+
+def main():
+    
+    a = 1
+    b = 2
+
+    print("sum is {0}".format(my_sum(a,b)))
+
+if __name__ == "__main__":
+    main()
+
