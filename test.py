2d87d76ec10462da8cf839bae9efa66aad690aea
SyogoMochida <syogo1207@gmail.com>
2020-08-15 15:56:54 +0900

    commitしたよ

--- test.py
+++ test.py
@@ -7,12 +7,17 @@ def my_sum(a,b):
     
     return a+b
 
+def my_dif(a,b):
+
+    return a-b
+
 def main():
     
     a = 1
     b = 2
 
     print("sum is {0}".format(my_sum(a,b)))
+    print("dif is {0}".format(my_dif(a,b)))
 
 if __name__ == "__main__":
     main()
